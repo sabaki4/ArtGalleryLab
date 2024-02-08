59a2621a15df92335528b88c169de41e7c786f3c
Saba Kia <skia6589@gmail.com>
2024-02-08 19:00:31 +0000

    created gallery class

---
+++ src/main/java/Gallery.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Gallery {
+
+    private String name;
+    private int till;
+    private ArrayList<Artwork> stock;
+
+    public Gallery(String inputName){
+        this.name = inputName;
+        this.till = 0;
+        this.stock = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTill() {
+        return till;
+    }
+
+    public void setTill(int till) {
+        this.till = till;
+    }
+
+    public ArrayList<Artwork> getStock() {
+        return stock;
+    }
+
+    public void setStock(ArrayList<Artwork> stock) {
+        this.stock = stock;
+    }
+}
