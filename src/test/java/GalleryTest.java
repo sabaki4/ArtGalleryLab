0cad29f67095c2d32caba703bceff26dac27a509
Saba Kia <skia6589@gmail.com>
2024-02-08 19:36:06 +0000

    gallery class canGetName test

--- src/test/java/GalleryTest.java
+++ src/test/java/GalleryTest.java
@@ -1,12 +1,27 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class GalleryTest {
 
     Gallery gallery;
-
+    Artist artist1;
+    Artist artist2;
+    Artwork artwork1;
+    Artwork artwork2;
 
     @BeforeEach
     public void setUp(){
         gallery = new Gallery("Serpentine");
+        artist1 = new Artist("Bosch");
+        artist2 = new Artist("Van Gogh");
+        artwork1 = new Artwork("Heavenly Delights", artist1, 500);
+        artwork2 = new Artwork("Flowers", artist2, 700);
+    }
+
+    @Test
+    public void canGetName(){
+        assertThat(gallery.getName()).isEqualTo("Serpentine");
     }
 }
