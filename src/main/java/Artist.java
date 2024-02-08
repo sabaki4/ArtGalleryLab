da2d48d5d7c2b70f1d725d8ca8aeacce522a3e7a
Saba Kia <skia6589@gmail.com>
2024-02-08 16:21:15 +0000

    getName, create artist constructor

--- src/main/java/Artist.java
+++ src/main/java/Artist.java
@@ -1,6 +1,10 @@
 public class Artist {
     private String name;
 
+    public Artist(String inputName){
+        this.name = inputName;
+    }
+
     public String getName() {
         return name;
     }
--- src/test/java/ArtistTest.java
+++ src/test/java/ArtistTest.java
@@ -1,3 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class ArtistTest {
-    
+    Artist artist;
+
+    @BeforeEach
+    public void setUp(){
+        artist = new Artist("Hannah");
+    }
+
+    @Test
+    public void canGetName(){
+        String actual = artist.getName();
+        assertThat(actual).isEqualTo("Hannah");
+    }
 }
